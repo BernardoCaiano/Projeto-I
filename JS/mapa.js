a26f0007cfdd9d95a163302194aa826fe1171415
Bernardo Ferreira <bernardo.caiano@outlook.pt>
2018-07-08 15:14:09 +0100

    continuacao do projeto para melhoria

--- JS/mapa.js
+++ JS/mapa.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 bibliotecasStorage()
 
 let map
@@ -18,7 +18,7 @@ let myMap = document.getElementById("myMap")
                 position: { lat: parseFloat(bibliotecas[i].latitude), lng: parseFloat(bibliotecas[i].longitude) },
                 //label: "00",
                 map: map,
-                //animation: google.maps.Animation.BOUNCE,
+                animation: google.maps.Animation.BOUNCE,
                 title: bibliotecas[i].morada
             
             })
@@ -39,45 +39,4 @@ let myMap = document.getElementById("myMap")
             }
         } 
     }    
-=======
-bibliotecasStorage()
 
-let map
-let myMap = document.getElementById("myMap")
-
-    function initMap() {
-        let mapProp= {
-            center:new google.maps.LatLng(41.373831, -8.741709),
-            mapTypeId: google.maps.MapTypeId.HYBRID,
-            zoom:13
-        }
-
-        map = new google.maps.Map(myMap, mapProp)
-                    
-        for (let i = 0; i < bibliotecas.length; i++) {
-            let marker = new google.maps.Marker({
-                position: { lat: parseFloat(bibliotecas[i].latitude), lng: parseFloat(bibliotecas[i].longitude) },
-                //label: "00",
-                map: map,
-                //animation: google.maps.Animation.BOUNCE,
-                title: bibliotecas[i].morada
-            
-            })
-    
-        }
-        
-    }
-
-    function bibliotecasStorage(){
-    
-        if(localStorage.bibliotecas) {
-            let tempArray = JSON.parse(localStorage.getItem("bibliotecas"))
-        
-            for (let i = 0; i < tempArray.length; i++) {
-            
-                let novaBiblioteca =  new Biblioteca (tempArray[i]._id, tempArray[i]._freguesia,  tempArray[i]._morada, tempArray[i]._latitude, tempArray[i]._longitude)
-                bibliotecas.push(novaBiblioteca)       
-            }
-        } 
-    }    
->>>>>>> d4c3ecd6ca2e0ef1e8717c0d250affedff895ae4
